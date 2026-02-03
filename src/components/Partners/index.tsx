6e2afd54cef23af4b899bfc698fa8d90663b8744
Zbrotskyi <azbrotskyi@genomics.org.ua>
2026-02-03 19:25:12 +0200

    Visual fix 2

--- src/components/Partners/index.tsx
+++ src/components/Partners/index.tsx
@@ -3,23 +3,27 @@ import Image from "next/image";
 const partnersData = [
     {
         id: 1,
-        name: "Partner 1",
-        logo: "/images/Partners/IP_off.png",
+        name: "YRiA",
+        logo: "/images/Partners/YRiA.png",
+        scale: "h-[55px] md:h-[70px]",
     },
     {
         id: 2,
-        name: "Partner 2",
+        name: "KSE",
         logo: "/images/Partners/KSE.png",
+        scale: "h-[50px] md:h-[65px]",
     },
     {
         id: 3,
-        name: "Partner 3",
+        name: "MES",
         logo: "/images/Partners/MES.png",
+        scale: "h-[55px] md:h-[70px]",
     },
     {
         id: 4,
-        name: "Partner 4",
-        logo: "/images/Partners/YRiA.png",
+        name: "IP_off",
+        logo: "/images/Partners/IP_off.png",
+        scale: "h-[40px] md:h-[50px]", // Reduced relative scale as it was too large
     },
 ];
 
@@ -27,18 +31,16 @@ const Partners = () => {
     return (
         <section className="bg-[#060607] py-8 md:py-12">
             <div className="container">
-                <div className="flex flex-wrap items-center justify-center gap-8 md:gap-16 lg:gap-24">
+                <div className="flex flex-wrap items-center justify-center gap-10 md:gap-20">
                     {partnersData.map((partner) => (
                         <div
                             key={partner.id}
-                            className="relative flex items-center justify-center opacity-60 transition-opacity duration-300 hover:opacity-100"
+                            className="relative flex items-center justify-center transition-all duration-300"
                         >
-                            <Image
+                            <img
                                 src={partner.logo}
                                 alt={partner.name}
-                                width={140}
-                                height={60}
-                                className="max-h-[50px] w-auto"
+                                className={`w-auto object-contain ${partner.scale}`}
                             />
                         </div>
                     ))}
