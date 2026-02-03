248455bbe9c6a1a71da5126fc5dc483e67d092f4
Zbrotskyi <azbrotskyi@genomics.org.ua>
2026-02-03 21:40:40 +0200

    Mobile fix 1

--- src/components/Partners/index.tsx
+++ src/components/Partners/index.tsx
@@ -5,7 +5,7 @@ const partnersData = [
         id: 1,
         name: "YRiA",
         logo: "/images/Partners/YRiA.png",
-        scale: "h-[80px] md:h-[100px]", // Scaled up even more
+        scale: "h-[65px] md:h-[85px]", // Scaled up
     },
     {
         id: 2,
@@ -29,7 +29,7 @@ const partnersData = [
 
 const Partners = () => {
     return (
-        <section className="relative z-20 -mt-36 bg-transparent py-8 md:py-12 overflow-hidden">
+        <section className="relative z-20 -mt-28 bg-transparent py-8 md:py-12 overflow-hidden">
             {/* Desktop View */}
             <div className="container hidden md:block">
                 <div className="flex flex-wrap items-center justify-center gap-10 md:gap-20">
@@ -48,13 +48,27 @@ const Partners = () => {
                 </div>
             </div>
 
-            {/* Mobile Dynamic Ribbon - Seamless Loop */}
-            <div className="flex w-full md:hidden">
-                <div className="flex animate-marquee whitespace-nowrap items-center">
-                    {[...partnersData, ...partnersData].map((partner, index) => (
+            {/* Mobile Dynamic Ribbon */}
+            <div className="flex w-full md:hidden overflow-hidden">
+                <div className="flex gap-12 items-center animate-marquee">
+                    {partnersData.map((partner) => (
                         <div
-                            key={`${partner.id}-${index}`}
-                            className="relative flex flex-shrink-0 items-center justify-center px-10"
+                            key={partner.id}
+                            className="relative flex flex-shrink-0 items-center justify-center px-4"
+                        >
+                            <img
+                                src={partner.logo}
+                                alt={partner.name}
+                                className={`w-auto object-contain ${partner.scale}`}
+                            />
+                        </div>
+                    ))}
+                </div>
+                <div className="flex gap-12 items-center animate-marquee" aria-hidden="true">
+                    {partnersData.map((partner) => (
+                        <div
+                            key={`${partner.id}-duplicate`}
+                            className="relative flex flex-shrink-0 items-center justify-center px-4"
                         >
                             <img
                                 src={partner.logo}
