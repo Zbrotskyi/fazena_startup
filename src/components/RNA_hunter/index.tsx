65ec3ac879db8c4488c44af3d35d27a712ed29cd
Zbrotskyi <azbrotskyi@genomics.org.ua>
2026-02-08 18:22:32 +0200

    new rna fix 2

--- src/components/RNA_hunter/index.tsx
+++ src/components/RNA_hunter/index.tsx
@@ -5,8 +5,17 @@ import SectionTitle from "../Common/SectionTitle";
 
 const RNAHunter = () => {
     return (
-        <section className="relative z-10 py-16 md:py-20 lg:py-28 bg-[#060607] bg-no-repeat bg-center bg-cover" style={{backgroundImage: `url('/images/video/shape.svg')`}}>
-            <div className="container">
+        <section className="relative z-10 py-16 md:py-20 lg:py-28 bg-[#060607]">
+            <div className="absolute inset-x-0 top-0 -z-10 h-full">
+                <Image
+                    src="/images/video/shape.svg"
+                    alt="Background shape"
+                    fill
+                    className="object-cover w-full h-full"
+                    priority
+                />
+            </div>
+            <div className="container relative">
                 <SectionTitle
                     title="RNA Hunter"
                     paragraph="Obtaining the spatial structure of non-coding RNAs and finding therapeutic small molecules for this target. Modelling in intracellular conditions, molecular dynamics, refinement of the lead molecule using QM/MM methods, calculation of free binding energy and other parameters. The platform is not limited by target length."
@@ -16,14 +25,14 @@ const RNAHunter = () => {
                 />
 
                 <div className="relative">
-                    <div className="mx-auto max-w-[1100px]">
+                    <div className="mx-auto max-w-[900px]">
                         <div className="relative overflow-hidden rounded-2xl border border-white/[0.08] shadow-[0_20px_60px_rgba(0,0,0,0.5)]">
                             <Image
                                 src="/images/video/image.png"
                                 alt="RNA Hunter platform screenshot"
                                 width={1280}
                                 height={720}
-                                className="mx-auto h-auto object-contain"
+                                className="w-full h-auto object-contain"
                             />
                         </div>
                     </div>
