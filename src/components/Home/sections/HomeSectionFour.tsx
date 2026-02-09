1c225955746c141c47772dbbeb9d5133c8c27980
Zbrotskyi <azbrotskyi@genomics.org.ua>
2026-02-09 16:16:49 +0200

    CycloCard fix 1

--- src/components/Home/sections/HomeSectionFour.tsx
+++ src/components/Home/sections/HomeSectionFour.tsx
@@ -24,8 +24,19 @@ const HomeSectionFour = () => {
                 <div className="border-b border-white/[.1] pb-16 md:pb-20 lg:pb-28">
                     <div className="-mx-4 flex flex-wrap items-center">
                         {/* Animation column (Left on Desktop) */}
-                        <div className="w-full px-4 lg:w-1/2 order-2 lg:order-1 flex justify-center">
-                            <CycloSpaceCard />
+                        <div className="w-full px-4 lg:w-1/2 order-2 lg:order-1 flex justify-center relative min-h-[400px]">
+                            <div className="absolute inset-0 z-0 opacity-40">
+                                <LetterGlitch
+                                    glitchColors={["#f2721c", "#fff47a", "#f9a443"]}
+                                    glitchSpeed={40}
+                                    centerVignette
+                                    outerVignette
+                                    smooth
+                                />
+                            </div>
+                            <div className="relative z-10 w-full flex justify-center items-center">
+                                <CycloSpaceCard />
+                            </div>
                         </div>
 
                         {/* Text column (Right on Desktop) */}
