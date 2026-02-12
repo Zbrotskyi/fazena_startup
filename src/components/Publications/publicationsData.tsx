fbb6012f3970c15742b114d5b2f2c7af9ea6756d
Zbrotskyi <azbrotskyi@genomics.org.ua>
2026-02-12 20:35:55 +0200

    Pub update

--- src/components/Publications/publicationsData.tsx
+++ src/components/Publications/publicationsData.tsx
@@ -4,7 +4,7 @@ const publicationsData: Publication[] = [
     {
         id: 1,
         title: "Discovery of a Druggable Ligand-Binding Site in Phospholipid-Transporting ATPase ABCA7",
-        image: "/images/publications/CERT1_2.jpg",
+        image: "/images/publications/ABCA7.jpg",
         publishDate: "Coming Soon",
         url: "in-future",
         tags: ["Research"]
@@ -12,7 +12,7 @@ const publicationsData: Publication[] = [
     {
         id: 2,
         title: "Rational in silico design of a peptide binder that inhibits CERT1",
-        image: "/images/publications/pub-02.jpg",
+        image: "/images/publications/CERT1_2.jpg",
         publishDate: "October 23, 2025",
         url: "https://www.researchgate.net/publication/397254517_Rational_in_silico_design_of_a_peptide_binder_that_inhibits_CERT1",
         tags: ["Research"]
@@ -24,22 +24,6 @@ const publicationsData: Publication[] = [
         publishDate: "October 23, 2025",
         url: "https://www.researchgate.net/publication/397496892_Rational_in_silico_redesign_of_the_CERT1_protein",
         tags: ["Research"]
-    },
-    {
-        id: 4,
-        title: "Future Breakthrough in Molecular Dynamics",
-        image: "",
-        publishDate: "Coming Soon",
-        url: "in-future",
-        tags: ["News"]
-    },
-    {
-        id: 5,
-        title: "FAZENA's Latest Strategic Partnerships for 2026",
-        image: "",
-        publishDate: "Coming Soon",
-        url: "in-future",
-        tags: ["Announcements"]
     }
 ];
 
